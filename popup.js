137ab478b7afbaf512e8215c427301446f09fdbf
tarikjones <tarikjones.git@gmail.com>
2024-07-29 19:23:32 -0400

    Add context menu for making phone numbers clickable

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('makeClickable').addEventListener('click', () => {
-    chrome.scripting.executeScript({
-      target: { allFrames: true },
-      files: ['content.js']
-    });
-  });
-  
